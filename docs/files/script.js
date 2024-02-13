ec1bce7ac536e7b9cf0c774d2e08ea128d3e00d3
maxstrikkers <maxstrikkers@outlook.com>
2024-02-13 22:25:42 +0100

    JSON File updated

--- docs/files/script.js
+++ docs/files/script.js
@@ -1,4 +1,4 @@
-const menuItems = document.querySelector(".menuIcon"); //Maakt een constante  aan menuItems van het item wat .menuIcon als class heeft
+const menuItems = document.querySelector(".menuIcon");
 const menu = document.querySelector(".menu");
 
 
@@ -9,38 +9,145 @@ function toggleMenu(){
     if (menu.classList.contains("hidden")){ //Kijkt of de constante menu .hidden classnaam heeft
         menu.classList.remove("hidden"); //Haalt de classnaam .hidden weg uit te constante menu
         menu.classList.add("shown"); //Voegt de classnaam .shown toe aan de constante menu
-    } else if (menu.classList.contains("shown")){ //Kijkt of als de constante menu niet de classnaam .hidden heeft of hij wel de classnaam .shown heeft
+    } else if (menu.classList.contains("shown")){ //Kijkt of menu niet de classnaam .hidden heeft of hij wel de classnaam .shown heeft
         menu.classList.remove("shown"); //Haalt de classnaam .shown weg uit de constante menu
         menu.classList.add("hidden") //Voegt de classnaam .hidden toe aan de constante menu
     }
 }
 
-menuItems.addEventListener("click", toggleMenu); //Wacht tot er op de constante menuItems geklikt word en roept daarna de functie toggleMenu op
+menuItems.addEventListener("click", toggleMenu); //Wacht tot er op menuItems geklikt word en roept daarna de functie toggleMenu op
 
 async function siteInfo() { //Functie die de JSON file gaat ophalen en waardes gaat veranderen
     const response = await fetch("./files/siteInfo.json"); //Maakt de variabele response aan door het JSON bestand te fetchen (en te wachten tot het binnen is doormiddel van await)
     const siteJson = await response.json(); //Zet het JSON bestand wat binnen komt als text om naar een JSON
-    document.getElementById('namePlaceholder').innerText = siteJson.name; //Zoekt het element met de Id namePlaceholder op en past de waarde aan gebaseerd op wat er in de variabele siteJson staat met de key (idk of het ook zo heet in JSON) name
+    document.getElementById('namePlaceholder').innerText = siteJson.firstName + ' ' + siteJson.lastName; //Zoekt het element met de Id namePlaceholder op en past de waarde aan gebaseerd op wat er in de variabele siteJson staat met de key (idk of het ook zo heet in JSON) name
+    document.getElementById('aboutMeInformation').innerText = siteJson.bio;
+    document.getElementById('profielFoto').src = siteJson.avatar_url;
   }
 
-siteInfo() //Voert de functie uit
+siteInfo() //Voert de functie uit 
 
-const playButton = document.getElementById("playButton");
-const pauzeButton = document.getElementById("pauzeButton")
-const meerVreten = document.getElementById("meerVreten")
+// Functie om alle items uit de json file toe te voegen aan de pagina
+async function updatePlaylist(){
+    const response = await fetch("./files/playlistInfo.json");
+    const playlistInfoJSON = await response.json();
 
-function playAudio(){
-    meerVreten.currentTime = 8;
-    meerVreten.play();
-    }
+    // Voegt de naam van de playlist toe aan de html
+    document.getElementById("playlistName").innerText = playlistInfoJSON.name;
 
-function pauzeAudio(){
-    meerVreten.pause()
+    // Voegt de src toe aan de image (de cover van de playlist)
+    const img = document.getElementById("playlistCover");
+    img.src = playlistInfoJSON.playlistCoverUrl;
+
+    playlistInfoJSON.tracks.items.forEach(element => {
+        const songName = element.title;
+        const artistName = element.user.name;
+        const albumCoverUrl = element.artworkUrl;
+        const songList = document.getElementById('songList')
+        const songId = element.id;
+
+    
+        // Maakt het list item aan die uiteindelijk de song gaat bevatten en voegt een classnaam toe
+        const songContainer = document.createElement("li");
+        songContainer.classList.add('songContainer', 'toggleMusicPopup');
+        
+        // Maakt de div aan die de song gaat bevatten en voegt een classnaam toe
+        const song = document.createElement("div");
+        song.classList.add('song');
+        
+        // Maakt de div aan waar de titel en artiest in komen te staan en maakt vervolgens een h3 (song titel) en een p (artiest naam) aan
+        const songNameAndArtist = document.createElement("div");
+        const titleOfTheSong = document.createElement("h3");
+        const nameOfTheArtist = document.createElement("p");
+
+        // Maakt de afbeelding aan, voedt een classnaam toe en voegt de src en alt toe
+        const songCoverImg = document.createElement("img");
+        songCoverImg.classList.add("songCover");
+        songCoverImg.src = albumCoverUrl;
+        songCoverImg.alt = "song cover";
+
+        // Zorgt ervoor dat de song titel en de artiest naam de juiste waarde krijgen
+        titleOfTheSong.textContent = songName;
+        nameOfTheArtist.textContent = artistName;
+        
+        // Zorgt ervoor dat de titel van het nummer en de artiest in de div met de classnamen song en songContainer komen
+        songNameAndArtist.appendChild(titleOfTheSong);
+        songNameAndArtist.appendChild(nameOfTheArtist);
+
+        // Zorgt ervoor dat de div met de song titel en de artiest naam in de div met de classnaam songContainer komt en dat de cover er in komt
+        song.appendChild(songNameAndArtist);
+        song.appendChild(songCoverImg);
+
+        // Maakt de div aan die uiteindelijk het popup element wordt en voegt een classnaam toe
+        musicPlayerPopup = document.createElement("div");
+        musicPlayerPopup.classList.add("musicplayerPopup");
+
+        // Maakt de knop aan om de popup te sluiten en voegt een classnaam toe
+        const button = document.createElement("button");
+        button.classList.add("closeMusicPopup");
+        constSVGForButton = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        constSVGForButton.setAttributeNS(null, "width", "2rem");
+        constSVGForButton.setAttributeNS(null, "height", "2rem");
+        constSVGForButton.setAttributeNS(null, "viewBox", "0 0 15 15");
+
+        // Maak het path-element
+        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        path.setAttributeNS(null, "fill", "currentColor");
+        path.setAttributeNS(null, "d", "M3.64 2.27L7.5 6.13l3.84-3.84A.92.92 0 0 1 12 2a1 1 0 0 1 1 1a.9.9 0 0 1-.27.66L8.84 7.5l3.89 3.89A.9.9 0 0 1 13 12a1 1 0 0 1-1 1a.92.92 0 0 1-.69-.27L7.5 8.87l-3.85 3.85A.92.92 0 0 1 3 13a1 1 0 0 1-1-1a.9.9 0 0 1 .27-.66L6.16 7.5L2.27 3.61A.9.9 0 0 1 2 3a1 1 0 0 1 1-1c.24.003.47.1.64.27");
+
+        // Maakt de iframe aan en haalt de juiste id op
+        const soundcloudIframe = document.createElement("iframe");
+        soundcloudIframe.src = `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${songId}&sharing=false&color=%230e1321&`;
+        
+        // Voeg het path-element toe aan het SVG-element
+        constSVGForButton.appendChild(path);
+        // Voeg het SVG-element toe aan het button-element
+        button.appendChild(constSVGForButton);
+        // Voeg het button-element toe aan de div met de classnaam musicplayerPopup
+        musicPlayerPopup.appendChild(button);
+        // Voegt de soundcloudIframe toe aan de div met de classnaam musicplayerPopup
+        musicPlayerPopup.appendChild(soundcloudIframe);
+
+        // zorgt ervoor dat de title, artiest en afbeelding toe worden gevoegd aan het list item songContainer
+        songContainer.appendChild(song);
+        songContainer.appendChild(musicPlayerPopup);
+
+        // Voegt het list item toe aan de ul met de id songList
+        songList.appendChild(songContainer);        
+    });    
 }
 
-playButton.addEventListener("click", playAudio);
-pauzeButton.addEventListener("click", pauzeAudio)
+updatePlaylist()
 
-document.querySelector(".dropDownContainer").addEventListener("click", function(){
-    this.classList.toggle("open");
-});
+// Functie om de pop-up te verwerken
+
+async function toggleMusicPopup(){
+    await updatePlaylist();
+    const songPopupButtonOpen = document.querySelectorAll(".songContainer");
+
+    songPopupButtonOpen.forEach(function(currentButton) {
+        const songPopupButtonClose = currentButton.querySelector(".closeMusicPopup");
+        const musicPopup = currentButton.querySelector(".musicplayerPopup");
+        const waas = document.querySelector(".waas");
+
+        currentButton.addEventListener('click', function(){
+            musicPopup.classList.add("musicOpen");
+            waas.classList.add("musicOpen");
+        });
+
+        function closeMenu(){
+            let iframeElement = currentButton.querySelector('iframe');
+            let widget = SC.Widget(iframeElement);
+            widget.pause();
+            musicPopup.classList.remove("musicOpen");
+            waas.classList.remove("musicOpen");
+        }
+        songPopupButtonClose.addEventListener('click', function(event) {
+            event.stopPropagation();
+            closeMenu();
+        });
+        waas.addEventListener('click', closeMenu);
+    });
+}
+
+toggleMusicPopup();
